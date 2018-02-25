9d6f0fa782fa358e3501b5579d9f8f1ca70ed504
Alex <alex@alex-debian>
2018-02-25 22:44:04 +0700

    Begin to work with dualfish.

--- main.cpp
+++ main.cpp
@@ -90,9 +90,9 @@ namespace {
 		return fishCoord;
 	}
 
-	void Generate360Buffers(std::vector<glm::vec3> & vertexBufferData,
-						 std::vector<glm::vec2> & uvBufferData,
-						 std::vector<GLushort> & indexBufferData)
+	void GenerateOneFishBuffers(std::vector<glm::vec3> & vertexBufferData,
+								std::vector<glm::vec2> & uvBufferData,
+								std::vector<GLushort> & indexBufferData)
 	{
 		vertexBufferData.clear();
 		uvBufferData.clear();
@@ -127,12 +127,106 @@ namespace {
 				indexBufferData.insert(indexBufferData.end(), {bli, tli, tri, bli, tri, bri});
 			}
 	}
+
+	glm::vec2 sphere2dualfish(glm::vec2 coord)
+	{
+		const float FOV = glm::pi<float>(); // 180 degrees
+
+		const float longitude = glm::two_pi<float>() * (coord.x / 2 /*- 0.5f*/);
+		const float latitude  = glm::pi<float>() * (coord.y / 2/* - 0.5f*/);
+
+		const glm::vec3 vec3d = {
+			glm::cos(latitude) * glm::sin(longitude),
+			glm::cos(latitude) * glm::cos(longitude),
+			glm::sin(latitude)
+		};
+
+		const float theta = glm::atan(vec3d.z, vec3d.x);
+		const float phi = glm::atan(glm::sqrt(vec3d.x * vec3d.x + vec3d.z * vec3d.z), vec3d.y);
+		const float r = phi / FOV;
+
+		const glm::vec2 fishCoord {
+			0.25f + r * glm::cos(theta) / 2.0f,// + ((coord.x < -0.5f || coord.x >= 0.5f) ? 0.5f : 0.0f),
+			0.5f + r * glm::sin(theta)
+		};
+
+		return fishCoord;
+	}
+
+	glm::vec2 sphere2fish2(glm::vec2 coord)
+	{
+		bool const isFront = coord.x >= -0.5f && coord.x < 0.5f;
+
+		const float FOV = glm::pi<float>(); // 180 degrees
+
+		const float longitude = glm::two_pi<float>() * (coord.x / 2.0f - (isFront ? 0.0f : 0.5f));
+		const float latitude  = glm::pi<float>() * (coord.y / 2.0f/* - 0.5f*/);
+
+		const glm::vec3 vec3d = {
+			glm::cos(latitude) * glm::sin(longitude),
+			glm::cos(latitude) * glm::cos(longitude),
+			glm::sin(latitude)
+		};
+
+		const float theta = glm::atan(vec3d.z, vec3d.x);
+		const float phi = glm::atan(glm::sqrt(vec3d.x * vec3d.x + vec3d.z * vec3d.z), vec3d.y);
+		const float r = phi / FOV;
+
+		const glm::vec2 fishCoord {
+			(isFront ? 0.25f : 0.75f) + r * glm::cos(theta) / 2.0f,
+			0.5f + r * glm::sin(theta)
+		};
+
+		return fishCoord;
+	}
+
+	void GenerateDualFishBuffers(std::vector<glm::vec3> & vertexBufferData,
+								 std::vector<glm::vec2> & uvBufferData,
+								 std::vector<GLushort> & indexBufferData)
+	{
+		vertexBufferData.clear();
+		uvBufferData.clear();
+		indexBufferData.clear();
+
+		const size_t xStepCount = 250;
+		const size_t yStepCount = 250;
+
+		const float xvStep = 2.0f / xStepCount;
+		const float yvStep = 2.0f / yStepCount;
+
+		const float xuvStep = 1.0f / xStepCount;
+		const float yuvStep = 1.0f / yStepCount;
+
+		for (size_t xIndex = 0; xIndex <= xStepCount; ++xIndex)
+			for (size_t yIndex = 0; yIndex <= yStepCount; ++yIndex)
+			{
+				const glm::vec2 sphereCoord{-1.0f + xIndex * xvStep, -1.0f + yIndex * yvStep};
+//				const glm::vec2 fishCoord = sphere2dualfish(sphereCoord);
+				const glm::vec2 fishCoord = sphere2fish2(sphereCoord);
+
+
+				vertexBufferData.push_back({sphereCoord.x, sphereCoord.y, 0.0f});
+				uvBufferData.push_back({fishCoord.x, fishCoord.y});
+			}
+
+		for (size_t xIndex = 0; xIndex < xStepCount; ++xIndex)
+			for (size_t yIndex = 0; yIndex < yStepCount; ++yIndex)
+			{
+				const GLushort tli = xIndex + yIndex * (xStepCount + 1);
+				const GLushort tri = tli + 1;
+				const GLushort bli = xIndex + (yIndex + 1) * (xStepCount + 1);
+				const GLushort bri = bli + 1;
+				indexBufferData.insert(indexBufferData.end(), {bli, tli, tri, bli, tri, bri});
+			}
+	}
 }
 
 int main(int, char**)
 {
 //	RawImage const inTex = RawImage::LoadFromFile("/home/alex/360/cube_orig.bmp", 4096, 4096);
-	RawImage const inTex = RawImage::LoadFromFile("/home/alex/360/fish2sphere180.bmp", 4096, 4096);
+//	RawImage const inTex = RawImage::LoadFromFile("/home/alex/360/fish2sphere180.bmp", 4096, 4096);
+	RawImage const inTex = RawImage::LoadFromFile("/home/alex/360/dual.bmp", 8192, 4096);
+
 
 	// Initialise GLFW
 	if (!glfwInit()) {
@@ -173,7 +267,7 @@ int main(int, char**)
 	std::vector<glm::vec2> uvBufferData;
 	std::vector<GLushort> indexBufferData;
 
-	Generate360Buffers(vertexBufferData, uvBufferData, indexBufferData);
+	GenerateDualFishBuffers(vertexBufferData, uvBufferData, indexBufferData);
 
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
@@ -207,14 +301,14 @@ int main(int, char**)
 	glm::mat4 const mvp = CreateSimpleMPVMatrix();
 	GLuint const mvpId = glGetUniformLocation(programId, "MVP");
 
-//	size_t const fbWidth = 1200;
-//	size_t const fbHeight = 600;
+	size_t const fbWidth = 1200;
+	size_t const fbHeight = 600;
 
-//	auto const fbParams = CreateFrameBuffer(fbWidth, fbHeight);
+	auto const fbParams = CreateFrameBuffer(fbWidth, fbHeight);
 
 	do {
-//		glBindFramebuffer(GL_FRAMEBUFFER, fbParams.first);
-//		glViewport(0, 0, fbWidth, fbHeight);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbParams.first);
+		glViewport(0, 0, fbWidth, fbHeight);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -222,7 +316,7 @@ int main(int, char**)
 
 		glUniformMatrix4fv(mvpId, 1, GL_FALSE, &mvp[0][0]);
 
-		// Don't forget to bind input texture back
+		// Don't forget to bind input texture back after working with fb
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, textureId);
 		glUniform1i(samplerID, 0);
@@ -258,12 +352,12 @@ int main(int, char**)
 			(void*)0           // element array buffer offset
 		);
 
-//		RawImage(GetFBTexture(fbWidth, fbHeight), "rgb24", fbWidth, fbHeight).SaveToFile("1.png");
+		RawImage(GetFBTexture(fbWidth, fbHeight), "rgb24", fbWidth, fbHeight).SaveToFile("1.png");
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 
-//		break;
+		break;
 
 		// Swap buffers
 		glfwSwapBuffers(window);
